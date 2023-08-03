e9d45ca182d610823b9124f326efd0d1ae1a2459
Ajeet <124139353+ajt11101@users.noreply.github.com>
2023-08-03 23:43:02 +0530

    NoChange

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,49 +1,45 @@
-import { useToast } from '@chakra-ui/react';
-import React, { useState,useEffect } from 'react'
-import { ChatState } from '../Context/ChatProvider';
-import axios from 'axios';
+import { useToast } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { ChatState } from "../Context/ChatProvider";
+import axios from "axios";
 
 const MyChats = () => {
-   const [loggedUser, setLoggedUser] = useState();
-   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
+  const [loggedUser, setLoggedUser] = useState();
+  const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
 
-   const toast=useToast();
+  const toast = useToast();
 
-    const fetchChats = async () => {
-      // console.log(user._id);
-      try {
-        const config = {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        };
+  const fetchChats = async () => {
+    // console.log(user._id);
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
 
-        const { data } = await axios.get("/api/chats", config);
-        console.log(data);
-        setChats(data);
-      } catch (error) {
-        toast({
-          title: "Error Occured!",
-          description: "Failed to Load the chats",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "bottom-left",
-        });
-      }
-    };
+      const { data } = await axios.get("/api/chats", config);
+      console.log(data);
+      setChats(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the chats",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
-    useEffect(() => {
-      setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
-      fetchChats();
-      // eslint-disable-next-line
-    }, []);
+  useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+    fetchChats();
+    // eslint-disable-next-line
+  }, []);
 
-  return (
-    <div>
-      My Chats
-    </div>
-  )
-}
+  return <div>My Chats</div>;
+};
 
-export default MyChats
+export default MyChats;
