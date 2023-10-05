5a0bcfaf37c78704c419e0829c246c5f6249f0ee
Ajeet <124139353+ajt11101@users.noreply.github.com>
2023-10-05 18:19:05 +0530

    bugs fixed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 //{router.route("/").post(registerUser).get(allUsers);
 //router.route("/").get(allUsers);}
 //These two lines can be combined into a single line as shown below.
+//protect is a middleware here.
 router.route("/").post(registerUser).get(protect,allUsers);
 router.post("/login",authUser);
 
